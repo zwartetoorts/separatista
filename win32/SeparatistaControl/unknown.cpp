b389c8e8db6c110b7fcfde795d2f8f57c60214d0
Okkel Klaver <info@vanhetland.nl>
2013-12-07 17:52:25 +0100

    Separated SepaControlDispatch and SepaControlUnknown

---
+++ win32/SeparatistaControl/unknown.cpp
@@ -0,0 +1,82 @@
+/***************************************************************************
+*   Copyright (C) 2013 by Okkel Klaver   *
+*   info@vanhetland.nl   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef SEPAUNKNOWN_CPP
+#define SEPAUNKNOWN_CPP
+
+#include <windows.h>
+#include "dll.h"
+#include "unknown.h"
+
+template <class T> SepaControlUnknown<T>::SepaControlUnknown(IUnknown *pParent)
+{
+	// Global
+	g_uDllRefCount++;
+
+	// Object
+	m_uRefCount = 0;
+
+	// Set parent
+	m_pParent = pParent;
+	if (pParent)
+		pParent->AddRef();
+}
+
+template <class T> SepaControlUnknown<T>::~SepaControlUnknown()
+{
+	g_uDllRefCount--;
+
+	// Check parent and release if necessary
+	if (m_pParent)
+		m_pParent->Release();
+}
+
+template <class T> ULONG SepaControlUnknown<T>::AddRef()
+{
+	return ++m_uRefCount;
+}
+
+template <class T> ULONG SepaControlUnknown<T>::Release()
+{
+	ULONG uRet;
+
+	uRet = --m_uRefCount;
+	if (0 == uRet)
+		delete this;
+	return uRet;
+}
+
+template <class T> HRESULT SepaControlUnknown<T>::QueryInterface(const IID &iid, void **pvvObject)
+{
+	*pvvObject = NULL;
+
+	if (IsEqualIID(iid, IID_IUnknown) ||
+		IsEqualIID(iid, __uuidof(T)))
+	{
+		*pvvObject = this;
+		AddRef();
+	}
+	else
+		return E_NOINTERFACE;
+
+	return S_OK;
+}
+
+#endif // !defined SEPAUNKNOWN_CPP
---
+++ win32/SeparatistaControl/unknown.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+*   Copyright (C) 2013 by Okkel Klaver   *
+*   info@vanhetland.nl   *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef SEPARATISTA_UNKNOWN_H
+#define SEPARATISTA_UNKNOWN_H
+
+#include <windows.h>
+
+template <class T>
+class SepaControlUnknown : public T
+{
+public:
+	/**
+		This constructor takes the parent COM object as an argument.
+		It will call AddRef on the parent object and call Release on destruction. This
+		will make sure the parent object isn't destroyed before this child.
+	*/
+	SepaControlUnknown(IUnknown *pParent = NULL);
+
+	// IUnknown methods
+	HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, void** pvvObject);
+	ULONG STDMETHODCALLTYPE AddRef();
+	ULONG STDMETHODCALLTYPE Release();
+
+protected:
+	/**
+		Destructor.
+		Calls Release on the parent COM object if set through the constructor.
+		Destructor with protected access. This object has to be freed through Release method
+		since it uses reference counting.
+		@see Release()
+	*/
+	virtual ~SepaControlUnknown();
+
+	ULONG m_uRefCount;
+	IUnknown *m_pParent;
+};
+
+#endif // !definded SEPARATISTA_UNKNOWN_H
